b995fe9a618f334d8781e2a89027c2d9d744e20a
anithaamarnath <anithaamarnath04@gmail.com>
2025-02-12 16:59:30 -0800

    retrying to fix cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,11 @@ require('dotenv').config();
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://budgetly-frontend.vercel.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+  }));
 
 app.use(express.json());
 
