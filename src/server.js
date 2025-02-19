adaff209ff42a3f17dbc4ec2be68befddebe3975
anithaamarnath <anithaamarnath04@gmail.com>
2025-02-18 22:34:31 -0800

    const value update

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
+const { DEFAULT_PORT_LOCAL_HOST } = require('./constants');
 const app = require('./app');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || DEFAULT_PORT_LOCAL_HOST;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
