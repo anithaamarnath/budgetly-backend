d3bf924c21f42652217e9e4424314b04d7e2cc11
anithaamarnath <anithaamarnath04@gmail.com>
2025-02-18 22:09:18 -0800

    login, const, remove console, comment

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,7 @@ const passwordComplexity = require("joi-password-complexity");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
+
 const transactionSchema = new mongoose.Schema({
   category: { type: String, required: true, enum: ["food", "transportation", "entertainment", "housing", "shopping"] },
   amount: { type: Number, required: true },
@@ -11,18 +12,19 @@ const transactionSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
 });
 
+
 const userTransactionSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   transactions: [transactionSchema],
   totalBudget: { type: Number, default: 0 },
   totalAmountSpent: { type: Number, default: 0 },
-  remainingBudget: {
-    type: Number,
-    default: function () {
-      return this.totalBudget - this.totalAmountSpent;
-    },
-  },
 });
+
+
+userTransactionSchema.virtual("remainingBudget").get(function () {
+  return this.totalBudget - this.totalAmountSpent;
+});
+
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 5, maxlength: 50 },
@@ -30,46 +32,24 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true, minlength: 8, maxlength: 1024 },
 });
 
-userSchema.methods.generateAuthToken = function () {
-  return jwt.sign({ _id: this._id }, process.env.JWT_PRIVATE_KEY);
-};
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-userTransactionSchema.pre("save", function (next) {
-  if (this.isModified("totalBudget") || this.isModified("totalAmountSpent")) {
-    this.remainingBudget = this.totalBudget - this.totalAmountSpent;
-  }
-  next();
-});
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign(
+    { _id: this._id, name: this.name, email: this.email },
+    process.env.JWT_PRIVATE_KEY,
+    { expiresIn: "1h" }
+  );
+};
+
 
 const User = mongoose.model("User", userSchema);
 const UserTransaction = mongoose.model("UserTransaction", userTransactionSchema);
 
-function validateUserTransaction(userTransaction) {
-  const schema = Joi.object({
-    user: Joi.string().required(),
-    transactions: Joi.array()
-      .items(
-        Joi.object({
-          category: Joi.string().valid("food", "transportation", "entertainment", "housing", "shopping").required(),
-          amount: Joi.number().greater(0).required(),
-          description: Joi.string().optional(),
-          date: Joi.date().iso().optional(),
-        })
-      )
-      .required()
-      .min(1),
-    totalBudget: Joi.number().greater(0).optional(),
-    totalAmountSpent: Joi.number().min(0).optional(),
-    remainingBudget: Joi.number().min(0).optional(),
-  });
-  return schema.validate(userTransaction);
-}
 
 function validateUser(user) {
   const schema = Joi.object({
@@ -90,6 +70,27 @@ function validateUser(user) {
     token: Joi.string().optional(),
   });
   return schema.validate(user);
+}
+
+
+function validateUserTransaction(userTransaction) {
+  const schema = Joi.object({
+    user: Joi.string().required(),
+    transactions: Joi.array()
+      .items(
+        Joi.object({
+          category: Joi.string().valid("food", "transportation", "entertainment", "housing", "shopping").required(),
+          amount: Joi.number().greater(0).required(),
+          description: Joi.string().optional(),
+          date: Joi.date().iso().optional(),
+        })
+      )
+      .required()
+      .min(1),
+    totalBudget: Joi.number().greater(0).optional(),
+    totalAmountSpent: Joi.number().min(0).optional(),
+  });
+  return schema.validate(userTransaction);
 }
 
 module.exports = { User, UserTransaction, validateUser, validateUserTransaction };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,4 @@
-const { STATUS_CODE_BAD_REQUEST, STATUS_CODE_INTERNAL_SERVER_ERROR, ZERO } = require('../constants');
+const { STATUS_CODE_BAD_REQUEST, STATUS_CODE_INTERNAL_SERVER_ERROR, ZERO,EXPIRES_IN_ONE_HOUR } = require('../constants');
 const _ = require('lodash');
 const express = require('express');
 const { User, validateUser } = require('../models/user');
@@ -11,10 +11,8 @@ router.post('/register', async (req, res) => {
   
     const { error } = validateUser(_.omit(req.body, ['token', 'isLoggedIn']));
     
-  
     if (error) return res.status(STATUS_CODE_BAD_REQUEST).send(error.details[ZERO].message);
    
-
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(STATUS_CODE_BAD_REQUEST).send('User already registered.');
 
@@ -23,17 +21,16 @@ router.post('/register', async (req, res) => {
         return res.status(STATUS_CODE_BAD_REQUEST).send('Passwords do not match.');
     }
     
-  
     user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
     try {
   
-        const salt = await bcrypt.genSalt(10);
+        const salt = await bcrypt.genSalt(process.env.SALT);
+
         user.password = await bcrypt.hash(user.password, salt);
-
+    
         await user.save();
-
-      
+       
         const token = user.generateAuthToken();
         res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
     } catch (err) {
@@ -46,21 +43,21 @@ router.post('/register', async (req, res) => {
 router.post('/signin', async (req, res) => {
     try {
         const { email, password } = req.body;
-   
-
-     
+        
         const user = await User.findOne({ email });
        
-        if (!user) return res.status(400).send('Invalid email or password.');
+        if (!user) return res.status(STATUS_CODE_BAD_REQUEST).send('Invalid email or password.');
+       
 
-        const validPassword = await bcrypt.compare(password, user.password);
-        if (!validPassword) return res.status(400).send('Invalid email or password.');
+        const validPassword = await bcrypt.compare(String(password), user.password);
+      
+        if (!validPassword) return res.status(STATUS_CODE_BAD_REQUEST).send('Invalid email or password.');
 
 
         const token = jwt.sign(
             { _id: user._id, email: user.email, name: user.name },
             process.env.JWT_PRIVATE_KEY, 
-            { expiresIn: '1h' } 
+            { expiresIn: EXPIRES_IN_ONE_HOUR } 
         );        
         
 
@@ -72,7 +69,7 @@ router.post('/signin', async (req, res) => {
 
     } catch (err) {
         console.error('Error during login:', err.message);
-        res.status(500).send('Server error');
+        res.status(STATUS_CODE_INTERNAL_SERVER_ERROR).send('Server error');
     }
 });
 
